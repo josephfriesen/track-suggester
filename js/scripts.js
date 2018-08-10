faca3d81dbec811816b5ddb5544b2b107887a1aa
Joseph Friesen <friesen.josephc@gmail.com>
2018-08-10 10:22:02 -0700

    Add function responseCount to business-side logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+/* Business-side logic */
+
+/*
+Given an array of strings stringArray and a single string stringToFind, the function responseCount will traverse the array and return the number of instances in which stringToFind appears in stringArray.
+  Ex: let array = ["hi", "hello", "hi"]
+          str = "hi"
+          responseCount(array, str) will return 2.
+Below, we will call this function on the variable qResponseArray three times to determine how many times the user selected option-1, option-2, and option-3 in the questionnaire.
+*/
+var responseCount = function(stringArray, stringToFind) {
+  var len = stringArray.length;
+  var count = 0;
+  var n = 0;
+  for ( ; n < len; ) {
+    if (stringArray[n] === stringToFind) {
+      count = count + 1;
+    }
+    n = n + 1;
+  };
+  console.log(count);
+  return count;
+};
+
+/*
+
+*/
+
+
+
+/* User interface logic */
+
 $(document).ready(function() {
   $("form#questionnaire").submit(function(event) {
     event.preventDefault();
@@ -9,6 +40,8 @@ $(document).ready(function() {
     var q3response = $("input:radio[name=question-3]:checked").val();
     var q4response = $("input:radio[name=question-4]:checked").val();
     var q5response = $("input:radio[name=question-5]:checked").val();
-    console.log(firstName, lastName, q1response, q2response, q3response, q4response, q5response);
+    var qResponseArray = [q1response, q2response, q3response, q4response, q5response];
+
+    console.log(firstName, lastName, q1response, q2response, q3response, q4response, q5response, qResponseArray);
   });
 });
